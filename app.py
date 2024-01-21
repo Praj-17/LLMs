7c76bfc1efc1d4039636fa1590291c211f601dc0
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2024-01-21 23:28:01 +0530

    Updated Readme and create chat with GPT API

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from question_generator import QuestionGenerator
+from question_generator import QuestionGenerator, ChatWithPDF
 from enum import Enum
 
 app = Flask(__name__)
@@ -28,7 +28,7 @@ class InputData:
         self.interval = interval
 
 @app.route("/", methods=["POST"])
-def run_function():
+def generate_questions():
     data = request.json
     print("entered")
     # data = []
@@ -68,6 +68,58 @@ def run_function():
             'message': message,
             'data': questions
         })
+@app.route("/chat", methods=["POST"])
+def chatwithpdf():
+    data = request.json
+    print("entered")
+    # data = []
+    answer = ""
+    status = True
+    reason = ""
+    url = data["url"]
+    page_no = 0
+    chat_history = []
+    if request.method == 'POST':
+
+        try:
+            chat_history = data.get('chat_history', [])
+            question = data.get("question", "")
+            if data["mode"] == 'all':
+                answer = chat.chat_with_whole_pdf(url=url, question=question, chat_history=chat_history)
+                status = False
+            elif data.get("page_number"):
+                if data["mode"] == 'single':
+                    answer = chat.chat_with_single_page(url=url, page_number=data["page_number"], question=question, chat_history=chat_history)
+                    status = False
+                elif data["mode"] == 'interval':
+                    answer = chat.chat_with_single_page(url=url, page_number=data["page_number"],  interval=data["interval"], question=question, chat_history=chat_history)
+                    status = False
+                else:
+                    reason = "Please provide  a correct mode"
+            else:
+                reason = "Please provide a param page_number"
+
+            print(answer)
+        except Exception as e:
+            reason = "Exception " + str(e)
+            print(reason)
+    
+
+        if status == False:
+            message = "Success"
+        else:
+            message = "Failure"
+        response_data = {
+        'guid': 0,
+        'answer':answer 
+    }
+        return jsonify({
+            'error': status,
+            # 'reason': reason,
+            'message': message,
+            'status': 200,
+            'response_data': response_data
+        })
 @app.route("/hello", methods=["GET"])
 def hello_world():
     return jsonify({
@@ -77,4 +129,5 @@ def hello_world():
 
 if __name__ == "__main__":
     generator = QuestionGenerator()
+    chat = ChatWithPDF()
     app.run(debug=True, host='127.0.0.1', port=8000)
--- readme.md
+++ readme.md
@@ -16,6 +16,8 @@
 HOST 
 `http://127.0.0.1:8000` (localhost)
 
+## Question Generation
+
 endpoint = `/get_mcq_questions`
 
 ### Input Parameters 
@@ -102,7 +104,51 @@ endpoint = `/get_mcq_questions`
     ]
 }
 ```
+## Chat With PDF
+
+endpoint = `/chat`
+
+### Input Parameters 
+
+1. mode = `['all', 'single', 'interval']` Any one of these 
+
+    a.`all`: When you want to extract entire text from a PDF 
+    b. `single`: When you want to extract text from a single page  
+    c. `interval`: When you want to extract +-n pages from a target page
+
+2. url = A string of `public` url for a PDF file hosted on a google drive  
+3. question = quetion asked by user 
+4. page_number  = Specific to `single` and `interval` format, specifies target page  
+5. interval = Specific to `interval` formatm specifies target interval  
+6. chat_history = A list of tuples with question answer pairs 
+
+#### input parameters
+```
+{
+    "url": "https://ncert.nic.in/textbook/pdf/kebo116.pdf",
+    "question": "what is this page about",
+    "mode": "interval",
+    "page_number": 2,
+    "interval":2,
+    "chat_history": [(question, answer), (question, answer)]
+}
+```
+
+#### Output format
+
+```
+{
+    "error": false,
+    "message": "Success",
+    "reason": "Exception name 'questions' is not defined",
+    "response_data": {
+        "answer": "This page is primarily about the functions and structures of the kidney, with a focus on the processes of filtration, reabsorption, and secretion. It details how the glomerulus filters blood and forms filtrate in the Bowman’s capsule, and how reabsorption of the filtrate takes place in different parts of the nephrons. It also discusses the role of the juxta glomerular apparatus (JGA) in regulating the Glomerular Filtration Rate (GFR). The page further explains the process of dialysis and kidney transplantation as methods for treating kidney failure. It also defines certain kidney conditions such as renal calculi and glomerulonephritis. The page includes a detailed explanation of the function of the tubules, including the Proximal Convoluted Tubule (PCT), Henle’s Loop, Distal Convoluted Tubule (DCT), and Collecting Duct. It also discusses the mechanism of concentration of the filtrate, particularly the role of Henle’s loop and vasa recta in producing a concentrated urine. Lastly, the page includes exercises and questions for further understanding of the topic.",
+        "guid": 0
+    },
+    "status": 200
+}
+```
     
-    
+
 
 
