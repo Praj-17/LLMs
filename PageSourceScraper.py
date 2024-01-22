17d7ea74faeda285ba018dd1bcbe1e1351d659b2
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2024-01-22 23:00:40 +0530

    Finalized the page source scraper, stored it in a new file, tested it, however it is problematic with the main meta page, other JS pages are loading, The problem is related to status code 403 that is Bad Request. Maybe because of too many attempts it has blocked me temporarily. Hopefully it will run from other devices.

---
+++ PageSourceScraper.py
@@ -0,0 +1,37 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+class PageSourceScraper:
+    def __init__(self) -> None:
+        # self.driver_path = "assets/chromedriver.exe"
+        service = Service()    
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        # self.driver = webdriver.Chrome(ChromeDriverManager().install(), service=service)
+    def get_url_source(self,url):
+        try:
+            self.driver.get(url)
+            return BeautifulSoup(self.driver.page_source, 'html.parser')
+        except Exception as e:
+            print("Exception: ", str(e))
+        finally:
+            self.driver.quit()
+    
+    def download_and_parse(self,url):
+        print("executing")
+        session = HTMLSession()
+        response = session.get(url)
+        response.html.render(timeout = 60)
+
+        # Get the rendered HTML content
+        page_source = response.html.html
+
+        # Use BeautifulSoup to parse the HTML
+        return BeautifulSoup(page_source, 'html.parser')
